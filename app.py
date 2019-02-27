7a4dc329d803771b4bb0676ccba7a651b17c224d
josktgui <jose.osk01@gmail.com>
2019-02-26 23:08:41 -0500

    adding route post

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response, make_response
+import json
 
 app = Flask(__name__)
 
@@ -14,12 +15,19 @@ books = [
   }
 ]
 
-# GET /books
+def validBookObject(bookObject):
+  if ('name' in bookObject and 'price' in bookObject and 'isbn' in bookObject):
+    return True
+  else:
+    return False
+
+############# ROUTES #############
+# GET
 @app.route('/books')
 def get_books():
   return jsonify({ 'books': books })
 
-# GET /books/<int:isbn>
+# GET
 @app.route('/books/<int:isbn>')
 def get_book_by_isbn(isbn):
   return_value = {}
@@ -32,5 +40,29 @@ def get_book_by_isbn(isbn):
       }
 
   return jsonify(return_value)
+
+# POST
+@app.route('/books', methods=['POST'])
+def add_book():
+  request_data = request.get_json()
+
+  if (validBookObject(request_data)):
+    new_book = {
+      'name': request_data['name'],
+      'price': request_data['price'],
+      'isbn': request_data['isbn']
+    }
+    books.insert(0, new_book)
+    response = Response('', 201, mimetype='application/json')
+    response.headers['Location'] = '/books/' + str(new_book['isbn'])
+    return response
+  else:
+    invalidBookObjectErrorMsg = {
+      'error': 'Invalid book object passed in request',
+      'helpString': 'Data passed in similar to this {"name": "bookname", "price", 4.88, "isbn": 232222}'
+    }
+    response = Response(json.dumps(invalidBookObjectErrorMsg), status = 400, mimetype='application/json')
+    return response
+
 
 app.run(port = 5000)
