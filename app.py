b3c2152a7937579cd41497e8c924306e2fe7f252
josktgui <jose.osk01@gmail.com>
2019-03-02 12:55:57 -0500

    saveing data to db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, Response
-import json
+from BookModel import *
 from settings import *
+import json
 
 books = [
   {
@@ -24,19 +25,11 @@ def validBookObject(bookObject):
 # GET
 @app.route('/books')
 def get_books():
-  return jsonify({ 'books': books })
+  return jsonify({ 'books': Book.get_all_books() })
 
 @app.route('/books/<int:isbn>')
 def get_book_by_isbn(isbn):
-  return_value = {}
-  for book in books:
-    if book['isbn'] == isbn:
-      return_value = {
-        'name': book['name'],
-        'price': book['price'],
-        'isbn': book['isbn']
-      }
-
+  return_value = Book.get_book(isbn)
   return jsonify(return_value)
 
 # POST
@@ -45,14 +38,9 @@ def add_book():
   request_data = request.get_json()
 
   if (validBookObject(request_data)):
-    new_book = {
-      'name': request_data['name'],
-      'price': request_data['price'],
-      'isbn': request_data['isbn']
-    }
-    books.insert(0, new_book)
+    Book.add_book(request_data['name'], request_data['price'], request_data['isbn'])
     response = Response('', 201, mimetype='application/json')
-    response.headers['Location'] = '/books/' + str(new_book['isbn'])
+    response.headers['Location'] = '/books/' + str(request_data['isbn'])
     return response
   else:
     invalidBookObjectErrorMsg = {
@@ -66,19 +54,7 @@ def add_book():
 @app.route('/books/<int:isbn>', methods=['PUT'])
 def replace_book(isbn):
   request_data = request.get_json()
-  new_book = {
-    'name': request_data['name'],
-    'price': request_data['price'],
-    'isbn': isbn
-  }
-  i = 0
-
-  for book in books:
-    currentItem = book['isbn']
-    if currentItem == isbn:
-      books[i] = new_book
-    i += 1
-
+  Book.replace_book(isbn, request_data['name'], request_data['price'])
   response = Response('', status=204)
   return response
 
@@ -86,17 +62,12 @@ def replace_book(isbn):
 @app.route('/books/<int:isbn>', methods=['PATCH'])
 def update_book(isbn):
   request_data = request.get_json()
-  updated_book = {}
 
   if ('name' in request_data):
-    updated_book['name'] = request_data['name']
+    Book.update_book_name(isbn, request_data['name'])
 
   if ('price' in request_data):
-      updated_book['price'] = request_data['price']
-
-  for book in books:
-    if book['isbn'] == isbn:
-      book.update(updated_book)
+    Book.update_book_price(isbn, request_data['price'])
   
   response = Response('', status=204)
   response.headers['Location'] = '/books/' + str(isbn)
@@ -105,14 +76,10 @@ def update_book(isbn):
 # DELETE
 @app.route('/books/<int:isbn>', methods=['DELETE'])
 def delete_book(isbn):
-  i = 0
-
-  for book in books:
-    if book['isbn'] == isbn:
-      books.pop(i)
-      response = Response('', status=204)
-      return response
-    i += 0
+  if (Book.delete_book(isbn)) {
+    response = Response('', status=204)
+    return response
+  }
   
   invalidBookObjectErrorMsg = {
     'msg': 'Book with the ISBN number that was provided was not found!'
